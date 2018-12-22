8829e4d9a9ed187a6c78173963a0b5197e45bab0
Nilo Soares <nilosoares@gmail.com>
2018-12-22 19:08:25 -0200

    Added the "ConnectPostgres" method

--- src/main/java/ConnectorHelper.java
+++ src/main/java/ConnectorHelper.java
@@ -1,11 +1,18 @@
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import java.sql.*;
 
 public class ConnectorHelper {
 
-    public DB connectMongo(String host, int port, String name) {
+    public DB connectMongo(String host, int port, String dbName) {
         MongoClient mongoClient = new MongoClient(host, port);
-        return mongoClient.getDB(name);
+        return mongoClient.getDB(dbName);
+    }
+
+    public Connection connectPostgres(String host, int port, String dbName) throws SQLException {
+        String url = "jdbc:postgresql://" + host + ":" + port + "/" + dbName + "?user=postgres&password=123456";
+        Connection conn = DriverManager.getConnection(url);
+        return conn;
     }
 
 }

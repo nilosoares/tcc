335d0b237004955f1e53b57d4370d57dca00e011
Nilo Soares <nilosoares@gmail.com>
2018-12-24 19:04:29 -0200

    Added the Q22

--- resources/tpc-h-postgres/queries/Q22.sql
+++ resources/tpc-h-postgres/queries/Q22.sql
@@ -25,7 +25,7 @@ from
 					and substring(c_phone from 1 for 2) in
 						('13', '31', '23', '29', '30', '18', '17')
 			)
-			and not exists (
+			and not exists ( -- Change this line to "and exists" to get the same result of MongoDB
 				select
 					*
 				from

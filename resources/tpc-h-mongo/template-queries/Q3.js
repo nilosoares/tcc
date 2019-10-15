56ffa31a4b34ad3f0a050503239214bc00e3885a
Nilo Soares <nilosoares@gmail.com>
2019-10-14 21:46:54 -0300

    Added Q3

---
+++ resources/tpc-h-mongo/template-queries/Q3.js
@@ -0,0 +1,53 @@
+(function() {
+
+    var finalDb = db.getSiblingDB("final");
+
+    var segment = "__PARAM_SEGMENT__";
+    var date = new Date(__PARAM_DATE__); // month is 0-indexed
+
+    return finalDb.deals.aggregate([
+        {
+            $match: {
+                "order.customer.mktsegment": { $eq: segment },
+                "order.orderdate": { $lt: date },
+                "shipdate": { $gt: date }
+            }
+        },
+        {
+            $group: {
+                "_id": {
+                    "o_orderkey": "$order.orderkey",
+                    "o_orderdate": "$order.orderdate",
+                    "o_shippriority": "$order.shippriority"
+                },
+                "revenue": {
+                    $sum: {
+                        $multiply: [
+                            "$extendedprice",
+                            { $subtract: [1, "$discount"] }
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                "_id": 0,
+                "o_orderkey": "$_id.o_orderkey",
+                "revenue": 1,
+                "o_orderdate": "$_id.o_orderdate",
+                "o_shippriority": "$_id.o_shippriority"
+            }
+        },
+        {
+            $sort: {
+                "revenue": -1,
+                "o_orderdate": 1
+            }
+        },
+        {
+            $limit: 10
+        }
+    ]);
+
+})();

9a7858dea4f271967dcccc3d7dfc96accb674452
Nilo Soares <nilosoares@gmail.com>
2019-10-19 20:20:10 -0300

    Added Q14

---
+++ src/main/java/Queries/Q14.java
@@ -0,0 +1,33 @@
+import java.nio.file.Path;
+import java.util.Calendar;
+
+class Q14 extends AbstractQuery {
+
+    public String getName() {
+        return "Q14";
+    }
+
+    public int getNbOfTests() {
+        return 9;
+    }
+
+    protected void replaceParameters() {
+        Path sTemplate = this.getScriptTemplate();
+        Path eTemplate = this.getExplainTemplate();
+
+        // Parameter 1 - Start Date
+        Calendar startDate = RandomHelper.getRandomDate(93, 0, 1, 97, 11, 1);
+        startDate.set(Calendar.DATE, 1);
+        FileSystemHelper.findAndReplace(sTemplate, "__PARAM_START_DATE__", DateHelper.jsFormat(startDate));
+        FileSystemHelper.findAndReplace(eTemplate, "__PARAM_START_DATE__", DateHelper.jsFormat(startDate));
+        LoggerHelper.addLog(this.getName(), "Parameter 1 (Start Date) = " + DateHelper.jsFormat(startDate));
+
+        // Parameter 2 - End Date (Date + 1 month)
+        Calendar endDate = (Calendar) startDate.clone();
+        endDate.add(Calendar.MONTH, 1);
+        FileSystemHelper.findAndReplace(sTemplate, "__PARAM_END_DATE__", DateHelper.jsFormat(endDate));
+        FileSystemHelper.findAndReplace(eTemplate, "__PARAM_END_DATE__", DateHelper.jsFormat(endDate));
+        LoggerHelper.addLog(this.getName(), "Parameter 2 (End Date) = " + DateHelper.jsFormat(endDate));
+    }
+
+}

098d4fc2fc03c150d05d57b5fe4f47c8bc8c5b75
Nilo Soares <nilosoares@gmail.com>
2020-06-08 09:13:15 -0300

    Update the getIndexes method of some queries

--- src/main/java/Queries/Q14.java
+++ src/main/java/Queries/Q14.java
@@ -31,7 +31,46 @@ class Q14 extends AbstractQuery {
     protected QueryIndexes getIndexes() {
         QueryIndexes names = new QueryIndexes();
 
-        names.add("P_10");
+        //names.add("P_10");
+        names.add("P_10_1");
+        names.add("P_10_2");
+        names.add("P_10_3");
+        names.add("P_10_4");
+        names.add("P_10_5");
+        names.add("P_10_6");
+        names.add("P_10_7");
+        names.add("P_10_8");
+        names.add("P_10_9");
+        names.add("P_10_10");
+        names.add("P_10_11");
+        names.add("P_10_12");
+        names.add("P_10_13");
+        names.add("P_10_14");
+        names.add("P_10_15");
+        names.add("P_10_16");
+        names.add("P_10_17");
+        names.add("P_10_18");
+        names.add("P_10_19");
+        names.add("P_10_20");
+        names.add("P_10_21");
+        names.add("P_10_22");
+        names.add("P_10_23");
+        names.add("P_10_24");
+        names.add("P_10_25");
+        names.add("P_10_26");
+        names.add("P_10_27");
+        names.add("P_10_28");
+        names.add("P_10_29");
+        names.add("P_10_30");
+        names.add("P_10_31");
+        names.add("P_10_32");
+        names.add("P_10_33");
+        names.add("P_10_34");
+        names.add("P_10_35");
+        names.add("P_10_36");
+        names.add("P_10_37");
+        names.add("P_10_38");
+        names.add("P_10_39");
 
         return names;
     }
--- src/main/java/Queries/Q19.java
+++ src/main/java/Queries/Q19.java
@@ -57,7 +57,23 @@ class Q19 extends AbstractQuery {
     protected QueryIndexes getIndexes() {
         QueryIndexes names = new QueryIndexes();
 
-        names.add("P_4_13_14");
+        //names.add("P_4_13_14");
+        names.add("P_4_13_14_1");
+        names.add("P_4_13_14_2");
+        names.add("P_4_13_14_3");
+        names.add("P_4_13_14_4");
+        names.add("P_4_13_14_5");
+        names.add("P_4_13_14_6");
+        names.add("P_4_13_14_7");
+        names.add("P_4_13_14_8");
+        names.add("P_4_13_14_9");
+        names.add("P_4_13_14_10");
+        names.add("P_4_13_14_11");
+        names.add("P_4_13_14_12");
+        names.add("P_4_13_14_13");
+        names.add("P_4_13_14_14");
+        names.add("P_4_13_14_15");
+        names.add("P_4_13_14_16");
         names.add("C_303");
 
         return names;
